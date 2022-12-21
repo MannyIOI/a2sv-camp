e4187f05b783a0c0c9d4440f974ba155476511bd
Amanuel T <aman.teferi.80@gmail.com>
2022-12-21 21:38:06 +0300

    Time: 244 ms (84.72%), Space: 18.3 MB (95.39%) - LeetHub

--- 0528-random-pick-with-weight/0528-random-pick-with-weight.py
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.py
@@ -1,14 +1,28 @@
-import random
-
 class Solution:
 
     def __init__(self, w: List[int]):
-        self.weights = w
-        self.indexes = [i for i in range(len(w))]
+        self.pSum = list(accumulate(w))
+        
 
     def pickIndex(self) -> int:
-        return random.choices(self.indexes, self.weights, k=1)[0]
+        target = random.random() * self.pSum[-1]
+        
+        low, high = 0, len(self.pSum)
+        
+        while low <= high:
+            mid = low + (high - low) // 2
+            if target > self.pSum[mid]:
+                low = mid + 1
+            else:
+                high = mid - 1
+        
+        return low
+        
+        
 
+# [1, 4, 8, 100]
+
+5
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(w)
 # param_1 = obj.pickIndex()
