5a746daec70c9535239ae76ff5e057898e72c4b1
Amanuel T <aman.teferi.80@gmail.com>
2022-12-21 04:30:12 +0300

    Time: 203 ms (25.98%), Space: 14 MB (51.05%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -3,7 +3,6 @@ class Solution:
         left, right = 0, 0
         diction = {}
         longest = 0
-        curr = longest
         
         while right < len(s):
             
@@ -12,7 +11,6 @@ class Solution:
             while diction.get(s[right], 0) > 1 and left <= right:
                 diction[s[left]] = diction.get(s[left], 0) - 1
                 left += 1
-                curr -= 1
             
             right += 1
             longest = max(right - left, longest)
