b69c8b5f3e9035e2fb99ba021f2f70ff197f6f67
Amanuel T <aman.teferi.80@gmail.com>
2023-01-21 23:11:00 +0300

    Time: 2487 ms (7.41%), Space: 14.1 MB (45.50%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.py
+++ 0649-dota2-senate/0649-dota2-senate.py
@@ -1,48 +1,15 @@
 class Solution:
     def predictPartyVictory(self, senate: str) -> str:
-        banned = ""
-        queue = deque([each for each in senate])
-        vote_freq = Counter(senate)
-        banned_count = 0
-        
-#         RRDDD
-        """
-        banned = ""
-        banned_count = 0
-        curr_vote = R
-        counter_freq = {}
-        
-        RRDDD
-        """
-    
-        while queue:
-            curr_vote = queue.popleft()
-            
-            if vote_freq['D'] == 0:
-                return 'Radiant'
-            elif vote_freq['R'] == 0:
-                return 'Dire'
-            
-            if banned_count > 0 and banned == curr_vote:
-                banned_count -= 1
-                vote_freq[banned] -= 1
-                continue
-            
-            banned = "R" if curr_vote == "D" else "D"
-            banned_count += 1
-            
-            queue.append(curr_vote)
-            
-            """
-            RRDDD
-            
-            curr_vote           D
-            vote_freq           {R: 1, D: 0}
-            banned_count        0
-            banned              D
-            
-            queue = [R
-            """
-            
-            
-            
+        hash={'R':'D','D':'R'}
+        ans={'R':'Radiant','D':'Dire'}
+        lst=list(senate)
+
+        while len(lst)>1:
+            try:
+                lst.append(lst[0])
+                lst=lst[1:]
+                #print(lst)
+                lst.remove(hash[lst[-1]])  
+            except:
+                return ans[lst[0]]
+        return ans[lst[0]]
